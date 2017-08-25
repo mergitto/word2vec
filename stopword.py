f95bf27a16c4e80d2a92e55a8a401bc4d8da9f41
mergitto <mfm17101@bene.fit.ac.jp>
2017-08-25 17:09:51 +0900

    ストップワードの削除とテキスト化

--- stopword.py
+++ stopword.py
@@ -1,10 +1,27 @@
 import urllib.request
-
+import pandas as pd
 
 f = urllib.request.urlopen('http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt')
 sw = [line.decode('utf-8').strip() for line in f] # 読み込んだurlから文章を読み込み
 sw = [ss for ss in sw if not ss==u''] # 空白を削除
 f.close()
 
-print(sw)
+wakati = pd.read_table("words-wakati1.txt", header=None)
+wakatiList = [w.strip() for w in wakati[0]]
+wakatiList = [ww for ww in wakatiList if not ww==u'']
+
+pdsw = [];
+for w in wakatiList:
+    stopRemove = []
+    words = w.split()
+    for word in words:
+        if not word in sw: # ストップワードを含んでいなければそのまま使う
+            stopRemove.append(word)
+    stopRemove = ','.join(stopRemove).replace(',', ' ') # カンマで区切り、カンマをスペースにすることで分かち書きの体裁を保つ
+    pdsw.append(stopRemove)
+
+f = open('stopword.txt', 'w')
+for x in pdsw:
+    f.write(str(x) + "\n")
+f.close()
 
