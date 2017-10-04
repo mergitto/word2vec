b5d80e1a2a4ed82ee0afca7d4c74159bd8fe8bf5
mergitto <mfm17101@bene.fit.ac.jp>
2017-10-04 15:01:24 +0900

    fastTextの類似度出力の修正

--- fastText/similar.py
+++ fastText/similar.py
@@ -2,4 +2,9 @@ from gensim.models.wrappers.fasttext import FastText
 import sys
 
 model = FastText.load_fasttext_format('model')
-print(model.most_similar(sys.argv[1]))
+result = model.most_similar(sys.argv[1])
+
+count = 1
+for r in result:
+    print(str(count) + "  " + str(r[0]) + "  " + str(r[1]))
+    count += 1
