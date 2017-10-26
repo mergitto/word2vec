291c927ec2be2b6cee782275b1f0107ea8b5336c
mergitto <mfm17101@bene.fit.ac.jp>
2017-10-26 18:04:39 +0900

    より汎用性を持たせた学習方法への修正

--- doc2vec/doc2vec.py
+++ doc2vec/doc2vec.py
@@ -4,44 +4,44 @@ import pandas as pd
 import MeCab
 import mojimoji
 
-df = pd.read_csv("~/Develop/word2vec/doc2vec/allAdvice.csv", header=None)
+df = pd.read_csv("~/Develop/word2vec/doc2vec/allAdviceKaiDrop.csv")
 
 # < >で囲まれた部分を削除する 例：<br />
-df[2] = df[2].str.replace("\<.+?\>", "")
+df['テキスト'] = df['テキスト'].str.replace("\<.+?\>", "")
 
 # 小文字化
-df[2] = df[2].str.lower()
+df['テキスト'] = df['テキスト'].str.lower()
 
 # [ ] で囲まれた部分は削除する 例：[就職活動のポイント ]
-df[2] = df[2].str.replace("\[.+?\]", "")
+df['テキスト'] = df['テキスト'].str.replace("\[.+?\]", "")
 
 
 # 半角から全角へ（数字を除く）
 pd.tmp = []
-for index, data in df[2].iteritems():
-    df[2][index]  = mojimoji.han_to_zen(mojimoji.zen_to_han(str(data), kana=False, ascii=False), digit=False)
+for index, data in df['テキスト'].iteritems():
+    df['テキスト'][index]  = mojimoji.han_to_zen(mojimoji.zen_to_han(str(data), kana=False, ascii=False), digit=False)
 
 # 同意義語の表記統一
-df[2] = df[2].str.replace("ｂｅｓｔ", "ベスト")
-df[2] = df[2].str.replace("ｓｕｃｃｅｓｓｓｑｉ", "サクセスｓｑｉ")
-df[2] = df[2].str.replace("ｅｌｓｅ", "ｅｌｓ")
-df[2] = df[2].str.replace("ｏｐｅｎｅｓ", "エントリーシート")
-df[2] = df[2].str.replace("ｏｐｅｎ　ｅｓ", "エントリーシート")
-df[2] = df[2].str.replace("ｏｅｓ", "エントリーシート")
-df[2] = df[2].str.replace("ｅｓ", "エントリーシート")
-df[2] = df[2].str.replace("ｓｅ", "システムエンジニア")
-df[2] = df[2].str.replace("ｇｄ", "グループディスカッション")
-df[2] = df[2].str.replace("ｈｐ", "ホームページ")
-df[2] = df[2].str.replace("ｐｒ", "ピーアール")
+df['テキスト'] = df['テキスト'].str.replace("ｂｅｓｔ", "ベスト")
+df['テキスト'] = df['テキスト'].str.replace("ｓｕｃｃｅｓｓｓｑｉ", "サクセスｓｑｉ")
+df['テキスト'] = df['テキスト'].str.replace("ｅｌｓｅ", "ｅｌｓ")
+df['テキスト'] = df['テキスト'].str.replace("ｏｐｅｎｅｓ", "エントリーシート")
+df['テキスト'] = df['テキスト'].str.replace("ｏｐｅｎ　ｅｓ", "エントリーシート")
+df['テキスト'] = df['テキスト'].str.replace("ｏｅｓ", "エントリーシート")
+df['テキスト'] = df['テキスト'].str.replace("ｅｓ", "エントリーシート")
+df['テキスト'] = df['テキスト'].str.replace("ｓｅ", "システムエンジニア")
+df['テキスト'] = df['テキスト'].str.replace("ｇｄ", "グループディスカッション")
+df['テキスト'] = df['テキスト'].str.replace("ｈｐ", "ホームページ")
+df['テキスト'] = df['テキスト'].str.replace("ｐｒ", "ピーアール")
 
 # 記号を削除
-df[2] = df[2].str.replace("[^ぁ-んァ-ンーa-zａ-ｚA-ZＡ-Ｚ0-9一-龠０-９\-\r]", "")
+df['テキスト'] = df['テキスト'].str.replace("[^ぁ-んァ-ンーa-zａ-ｚA-ZＡ-Ｚ0-9一-龠０-９\-\r]", "")
 
 
 # mecab
 me = MeCab.Tagger('-Owakati')
 wakachi_list = []
-for i, di in enumerate(df[2]):
+for i, di in enumerate(df['テキスト']):
     try:
         w = me.parse(di)
     except:
@@ -59,9 +59,10 @@ dfwakati = pd.concat([df, wakati], axis=1)
 
 training_code = []
 for i in dfwakati.iterrows():
-    training_code.append(TaggedDocument(words=i[1]['wakati'].split(), tags=[str(i[1][1])]))
+    training_code.append(TaggedDocument(words=i[1]['wakati'].split(), tags=[str(i[1]['企業名'])]))
 
-model = Doc2Vec(documents=training_code, size=100 , window=10, min_count=1, dm=1, iter=50)
+# dm=1 DBOW dm=0 dmpv
+model = Doc2Vec(documents=training_code, size=300 , window=15, min_count=1, dm=0, iter=400, negative=5, sample=1e-6)
 
-model.save('./doc2vec-ws-10-epo-50.model')
+model.save('~/Develop/word2vec/doc2vec/dmpv-ws-15-epo-400-ns-5.model')
 
